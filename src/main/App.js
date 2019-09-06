74755f27aa89a0f8fa703bc74da56d4675db7338
José Negrão <joseolimpionegrao@gmail.com>
2019-09-06 16:38:16 -0300

    Update App.js

--- src/main/App.js
+++ src/main/App.js
@@ -1,5 +1,22 @@
 /**
- * Importação dos módulos a serem utilizados nesse arquivo JS
+
+LODGvis -Linked Open Data Graph Visualization, 
+Copyright (C) 2019  Danilo Coimbra, José Negrão.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+* Importação dos módulos a serem utilizados nesse arquivo JS
  */
 import React, { Component } from 'react';//React e classe Component, necessários para se criar um component que possa ser interpretado pelo React
 import Graph from 'react-graph-vis';//React-graph-vis, Componente do criado para exibir grafos do Vis em React. https://github.com/crubier/react-graph-vis
